ac393bec14baeebea2b3ae2a85865a2f5cad8b9d
foxvip <bruce1227@gmail.com>
2025-06-27 15:36:45 +0800

    Update update.sh
    Signed-off-by: foxvip <bruce1227@gmail.com>

--- update.sh
+++ update.sh
@@ -863,7 +863,7 @@ main() {
     update_lucky
     fix_rust_compile_error
     update_smartdns_luci
-    update_diskman
+    #update_diskman
     install_feeds
     support_fw4_adg
     update_script_priority
@@ -871,8 +871,8 @@ main() {
     update_geoip
     update_package "runc" "releases" "v1.2.6"
     update_package "containerd" "releases" "v1.7.27"
-    update_package "docker" "tags" "v28.2.2"
-    update_package "dockerd" "releases" "v28.2.2"
+    #update_package "docker" "tags" "v28.2.2"
+    #update_package "dockerd" "releases" "v28.2.2"
     # update_package "xray-core"
     # update_proxy_app_menu_location
     # update_dns_app_menu_location
